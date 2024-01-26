ef38558a66d49fc48311213143cac02273eb6812
Deepanjali Singh <113447060+DEEPANJALI-11@users.noreply.github.com>
2024-01-27 00:41:44 +0530

    Add files via upload

--- first.sql
+++ first.sql
@@ -35,6 +35,32 @@ select * from sales;
 select * from product;
 select * from goldusers_signup;
 
+-- q1. what is the the total amount spent by customers? 
+select a.user_id,sum(b.price ) from sales a inner join product b on a.product_id=b.product_id
+group by user_id;
+-- q2.how many days has each customer visited the site?
+select user_id,count(distinct created_date) days from sales group by user_id;
+-- q3. first product purchased by customers?
+
+select *, rank() over( partition by user_id order by created_date) rnk from sales;
+-- getting the first rank product only
+select * from
+(select *, rank()over (partition by user_id order by created_date)rnk from sales) a
+where rnk =1;
+
+-- q4. most purchased product and its frequency
+select  product_id,count(product_id) cnt
+from sales
+group by product_id 
+order by cnt desc;
+-- which user has purchase most frequent product and how  many times
+select user_id,count(product_id) from sales where product_id=(select   product_id
+from sales
+group by product_id 
+order by count(product_id) desc
+limit 1)
+group by user_id;
+
 
 
 
